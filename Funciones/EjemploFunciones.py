9939e240cf44a428e24295ede33c751921147d3d
Nicolassandovalm <nicola.sandovalm@duocuc.cl>
2026-05-30 11:33:37 -0400

    funciones en otro archivo

--- Funciones/EjemploFunciones.py
+++ Funciones/EjemploFunciones.py
@@ -1,30 +1,14 @@
-#Funciones
-
-
-def sumarDosNumeros():
-    '''Esta función permite sumar dos números
-     ingresados dentro de la función'''
-    
-    num1 = int(input("Ingrese número 1: "))
-    num2 = int(input("Ingrese número 2: "))
-    suma = num1 + num2
-
-    print(f"La suma de {num1} + {num2} es = {suma}")
-
-
-def sumar(a,b):
-    '''Esta funcion permite sumar dos números 
-    ingresados por parametros'''
-    suma = a + b
-    return suma
+import funcionesDetalle as fn
+#from funcionDetalle import sumar
 
 #linea ppal
-sumarDosNumeros()
+fn.sumarDosNumeros()
 
-res = sumar(4,5)
+res = fn.sumar(4,5)
 print(res)
 
 num1 = int(input("Número 1: "))
-num2 = int(input("Nummero 2: "))
-res = sumar(num1,num2)
+num2 = int(input("Númmero 2: "))
+res = fn.sumar(num1,num2)
 print("La suma es: ",res)
+
---
+++ Funciones/funcionesDetalle.py
@@ -0,0 +1,19 @@
+#Funciones
+
+def sumarDosNumeros():
+    '''Esta función permite sumar dos números
+     ingresados dentro de la función'''
+    
+    num1 = int(input("Ingrese número 1: "))
+    num2 = int(input("Ingrese número 2: "))
+    suma = num1 + num2
+
+    print(f"La suma de {num1} + {num2} es = {suma}")
+
+
+def sumar(a,b):
+    '''Esta funcion permite sumar dos números 
+    ingresados por parametros'''
+    suma = a + b
+    return suma
+
